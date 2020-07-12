93089f170f8209d88537118891dd183cc2197856
Emre Sen <maemresen@yazilim.vip>
2020-07-12 12:23:31 +0300

    wip: navbar configured

--- src/App.js
+++ src/App.js
@@ -2,49 +2,73 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from "./page/Home";
 import About from "./page/About";
-import { Navbar, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Row, Col, Container } from 'react-bootstrap';
 
 import './App.css';
 
 const App = () => (
   <Router>
     <div>
-      <Navbar bg="dark" variant="dark" className="text-center">
-        <Navbar.Brand href="#home">
+
+      <div style={styles.navbar}>
+        <Col md={{ span: 12 }}>
           <Row>
-            <Col>
-              <img
-                alt=""
-                src="/logo.svg"
-                width="170"
-                className="d-inline-block align-top"
-              />{' '}
+            <Col md={{ span: 6, offset: 3 }}>
+
+              <Navbar bg="dark" variant="dark">
+
+                <Navbar.Brand href="#home">
+                  <img
+                    alt=""
+                    src="/logo.svg"
+                    width="170"
+                    className="d-inline-block align-top"
+                  />{' '}
+                  <span style={styles.slogan} className="text-white-50 d-block">
+                    Do the right, not the easy
+                  </span>
+                </Navbar.Brand>
+
+                <Nav className="mr-auto">
+                  <Nav.Link>
+                    <Link className="text-light" to={'/'}>Home</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link className="text-light" to={'/another'}>About</Link>
+                  </Nav.Link>
+                </Nav>
+
+              </Navbar>
             </Col>
           </Row>
+        </Col>
+      </div>
+
+      <div className="mt-4">
+        <Col md={{ span: 12 }}>
           <Row>
-            <Col>
-            <span className="text-white-50">
-              Do the right, not the easy
-            </span>
+            <Col md={{ span: 6, offset: 3 }}>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/another' component={About} />
+              </Switch>
             </Col>
           </Row>
-        </Navbar.Brand>
-      </Navbar>
-
-      <h2>React Router Example</h2>
-      <ul>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/another'}>About</Link></li>
-      </ul>
-      <hr />
-
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/another' component={About} />
-      </Switch>
+        </Col>
+      </div>
     </div>
 
   </Router>
 );
+
+const styles = {
+  slogan: {
+    fontSize: '15px'
+  },
+
+  navbar: {
+    backgroundColor: '#343a40'
+  }
+}
 
 export default App;
