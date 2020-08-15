018cf55f3b003f940c1267e2f8eaae621ff77985
Emre Sen <maemresen@yazilim.vip>
2020-08-15 14:42:15 +0300

    refacotr: redundant components removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,46 @@
+// ReactJS 
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Bootstrap
+import { Row, Col, Navbar } from 'react-bootstrap';
 
+// Pages
 import Home from "./page/Home";
 import About from "./page/About";
 
 import './App.css';
+import CustomNavbar from './component/CustomNavbar';
 
-import PageFooterComponent from './component/PageFooterComponent';
-import GridLayout from './layout/GridLayout';
-import PageHeaderComponent from './component/PageHeaderComponent';
 
 const App = () => (
   <Router>
 
-    <PageHeaderComponent />
-    
+    {/* HEADER */}
+    <header>
+      <CustomNavbar />
+    </header>
+
     {/* BODY */}
     <main className="my-4">
-      <GridLayout>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-        </Switch>
-      </GridLayout>
+      <Col md={{ span: 12 }}>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </Col>
+        </Row>
+      </Col>
     </main>
 
-    <PageFooterComponent />
+    {/* FOOTER */}
+    <footer>
+      <Navbar bg="dark" className="justify-content-center">
+        <span className="text-white-50">© yazilim.vip All Rights Reserved</span>
+      </Navbar>
+    </footer>
 
   </Router>
 );
