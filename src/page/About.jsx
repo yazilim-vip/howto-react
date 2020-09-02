8cb31750294eda15f607ec91db59410234b7d6a4
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-09-02 22:42:07 +0300

    impl: TeamMemberCard

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -2,38 +2,44 @@ import React from 'react';
 
 // Component
 import Page from '../component/Page';
-import TeamInfoComponent from '../component/TeamInfoComponent';
+import TeamMemberComponent from '../component/TeamMemberCard';
+
+const team_member_list = [
+    {
+        name: "Mehmet Arif Emre Sen",
+        title: "Major Contributor",
+        info: "Computer Engineer",
+        image_name: "https://gitlab.com/uploads/-/system/user/avatar/1102036/avatar.png?width=400"
+    },
+    {
+        name: "Burak Erkan",
+        title: "Major Contributor",
+        info: "Computer Engineer",
+        image_name: "https://gitlab.com/uploads/-/system/user/avatar/2674558/avatar.png?width=400"
+    },
+    {
+        name: "Mustafa Arif Sisman",
+        title: "Major Contributor",
+        info: "Computer Engineer",
+        image_name: "https://gitlab.com/uploads/-/system/user/avatar/1907363/avatar.png?width=400"
+    },
+    {
+        name: "Ahmet Gurdal",
+        title: "Contributor",
+        info: "Computer Engineer",
+        image_name: "https://secure.gravatar.com/avatar/93d5395c86f888c9e820f1805dc874f2?s=800&d=identicon"
+    }
+]
+
+const memberToComponent = (tm, i) => <TeamMemberComponent team_member={tm}> </TeamMemberComponent>
 
 const About = () => (
     <Page>
-        <TeamInfoComponent team_member_list={
-            [
-                {
-                    name: "Mehmet Arif Emre Sen",
-                    title: "Major Contributor",
-                    info: "Computer Engineer",
-                    image_name: "https://gitlab.com/uploads/-/system/user/avatar/1102036/avatar.png?width=400"
-                },
-                {
-                    name: "Burak Erkan",
-                    title: "Major Contributor",
-                    info: "Computer Engineer",
-                    image_name: "https://gitlab.com/uploads/-/system/user/avatar/2674558/avatar.png?width=400"
-                },
-                {
-                    name: "Mustafa Arif Sisman",
-                    title: "Major Contributor",
-                    info: "Computer Engineer",
-                    image_name: "https://gitlab.com/uploads/-/system/user/avatar/1907363/avatar.png?width=400"
-                },
-                {
-                    name: "Ahmet Gurdal",
-                    title: "Contributor",
-                    info: "Computer Engineer",
-                    image_name: "https://secure.gravatar.com/avatar/93d5395c86f888c9e820f1805dc874f2?s=800&d=identicon"
-                }
-            ]
-        } />
+        <div>
+            {
+                team_member_list.map(memberToComponent)
+            }
+        </div>
     </Page>
 )
 
--- src/page/Portfolio.jsx
+++ src/page/Portfolio.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 // Component
 import Page from '../component/Page';
+import PortfolioComponent from '../component/PortfolioComponent';
 
 const Portfolio = () => (
     <Page>
-
+        <PortfolioComponent portfolio_list={
+            [
+                {
+                    name: "Mehmet Arif Emre Sen",
+                    title: "Major Contributor",
+                    info: "Computer Engineer",
+                    image_name: "https://gitlab.com/uploads/-/system/user/avatar/1102036/avatar.png?width=400"
+                }
+            ]
+        } />
     </Page>
 )
 
