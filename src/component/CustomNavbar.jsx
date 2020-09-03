0fd21020b569c8cba8a43c53e5d89b88d76a75da
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-09-03 04:58:37 +0300

    impl: Navbar and PortfolioCard improvements

--- src/component/CustomNavbar.jsx
+++ src/component/CustomNavbar.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 
 const CustomNavbar = () => (
     <div style={styles.navbar}>
-        <Navbar bg="dark" variant="dark" className="justify-content-center">
-            <Row className="align-items-center">
-                <Col md={{ span: 12 }}>
-                    <Navbar.Brand className="mr-0">
-                        <img
-                            alt=""
-                            src="/logo.svg"
-                            width="300"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        <span style={styles.slogan} className="text-white-50 d-block">Do the right, not the easy</span>
-                    </Navbar.Brand>
-                    <Nav className="justify-content-center">
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand className="pl-5">
+            <img
+                alt=""
+                src="/logo.svg"
+                width="250"
+                className="d-inline-block align-top"
+            />
+            <span style={styles.slogan} className="text-white-50 d-block">Do the right, not the easy</span>
+        </Navbar.Brand>
+            <Row>
+                <Col>
+                    <Nav className="pl-5 ml-auto">
                         <Link className="text-light nav-link" to={'/'}>Home</Link>
                         <Link className="text-light nav-link" to={'/portfolio'}>Portfolio</Link>
                         <Link className="text-light nav-link" to={'/about'}>About</Link>
--- src/component/PortfolioCard.jsx
+++ src/component/PortfolioCard.jsx
@@ -9,11 +9,12 @@ const linkToIcon = (linkMap) => {
   const url = linkMap[1];
 
   return (
-    <a href={url} target="blank" className={`text-muted portfolio-link ${iconCode}`}>
-      <FontAwesomeIcon
-        icon={["fab", iconCode]}
-        className="mr-3"
-      />
+    <a
+      href={url}
+      target="blank"
+      className={`text-muted portfolio-link ${iconCode}`}
+    >
+      <FontAwesomeIcon icon={["fab", iconCode]} className="mr-3" />
     </a>
   );
 };
@@ -22,28 +23,34 @@ const PortfolioCard = (props) => {
   const portfolio = props.portfolio;
   const portfolioLinks = portfolio.links;
 
+  var portfolioFooterElement;
   var portfolioImgElement;
-  var portfolioLinksElement;
 
   if (portfolio.imageSource) {
     portfolioImgElement = (
-      <Card.Img variant="top" src={`${portfolio.imageSource}`}/>
+      <Card.Img variant="top" src={`${portfolio.imageSource}`} />
     );
   }
 
   if (portfolioLinks !== undefined && portfolioLinks !== null) {
-    portfolioLinksElement = Object.entries(portfolioLinks).map(linkToIcon);
+    portfolioFooterElement = (
+      <Card.Footer className="text-center">
+        {Object.entries(portfolioLinks).map(linkToIcon)}
+      </Card.Footer>
+    );
   }
 
   return (
-    <Card className="mb-4 portfolio-card shadow border border-dark">
+    <Card className="mb-4 portfolio-card shadow">
       {portfolioImgElement}
       <Card.Body>
         <Card.Title>{portfolio.name}</Card.Title>
-        <Card.Subtitle className="mb-4 text-muted">{props.portfolio.title}</Card.Subtitle>
+        <Card.Subtitle className="mb-4 text-muted">
+          {props.portfolio.title}
+        </Card.Subtitle>
         <Card.Text>{portfolio.description}</Card.Text>
       </Card.Body>
-      <Card.Footer className="text-center">{portfolioLinksElement}</Card.Footer>
+      {portfolioFooterElement}
     </Card>
   );
 };
