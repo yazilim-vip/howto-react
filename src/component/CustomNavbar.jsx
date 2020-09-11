78249c1b2fff6452f297e679fa26313a0c958e0d
Emre Sen <maemresen@yazilim.vip>
2020-09-11 05:23:45 +0300

    CI test

--- src/component/CustomNavbar.jsx
+++ src/component/CustomNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import YvipIcon from "./YvipIcon";
