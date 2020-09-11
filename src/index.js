be859f448302b6bd2bca70d5763e63af65a27058
Emre Sen <maemresen@yazilim.vip>
2020-09-11 05:08:17 +0300

    CI test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+import WebFont from 'webfontloader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 WebFont.load({
-    google: {
-      families: ['Lato', 'Ubuntu']
-    }
- });
+  google: {
+    families: ['Lato', 'Ubuntu'],
+  },
+});
