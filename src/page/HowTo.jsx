91e44296fda7561c34f3ff5e3bc2839f66b5e2fe
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-09-12 17:48:52 +0300

    impl(WIP): HowTo

--- src/page/HowTo.jsx
+++ src/page/HowTo.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 
-// Component
-import Page from "../component/Page";
-import { Card } from "react-bootstrap";
+class HowTo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
 
-const HowTo = (props) => (
-  <Page>
-    
-  </Page>
-);
+  componentDidMount() {
+    fetch("http://yazilim.vip:9999/howto")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
 
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
 
-export default HowTo;
+export default HowTo
