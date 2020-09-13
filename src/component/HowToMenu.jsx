0d09ae4432b97b1ab0163e7e794eaeda1dbe2336
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-09-13 21:46:42 +0300

    impl: HowToMenu component

---
+++ src/component/HowToMenu.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {ListGroup} from "react-bootstrap";
+
+const HowToMenu = (props) => {
+    const route = props.route;
+    const type = props.type;
+    const title = props.title;
+    const items = props.items;
+
+    const renderItem = (key) => {
+        switch (type) {
+            case "subcategory":
+                return (
+                    <ListGroup.Item key={key}>
+                        <a href={`/howto/${route}/${items[key].name}`} key={key}>
+                            {items[key].name}
+                        </a>
+                    </ListGroup.Item>
+                )
+            case "content":
+                return (
+                    <ListGroup.Item key={key}>
+                        <a href={`/howto/${route}/${items[key].label}`} key={key}>
+                            {items[key].label}
+                        </a>
+                    </ListGroup.Item>
+                )
+            default: return (<div/>)
+        }
+    }
+
+    const renderItems = Object.keys(items).map(key => {return (renderItem(key))})
+    const renderTitle = <h5 className="pl-3">{title}</h5>
+
+    return (
+        <div>
+            {Object.keys(items).length !== 0 ? renderTitle : null}
+
+            <ListGroup>
+                {items !== undefined ? renderItems : null}
+            </ListGroup>
+
+        </div>
+    );
+};
+
+export default HowToMenu;
--- src/page/HowTo.jsx
+++ src/page/HowTo.jsx
@@ -1,138 +1,117 @@
 import React from "react";
-import { Breadcrumb, Col, ListGroup, Row } from "react-bootstrap";
+import {Breadcrumb, Col, Row} from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
+import HowToMenu from "../component/HowToMenu";
 import Page from "../component/Page";
 
 class HowTo extends React.Component {
-  constructor(props) {
-    super();
+    constructor(props) {
+        super(undefined);
 
-    let categoryNames = props.match.params[0].split("/")
-    categoryNames.unshift("howto")
-    let categorySelectedFlag;
+        let categoryNames = props.match.params[0].split("/")
+        categoryNames.unshift("howto")
 
-    if (categoryNames.length === 1 && categoryNames[0] === "") {
-      categorySelectedFlag = false
-    } else {
-      categorySelectedFlag = true
+        this.state = {
+            error: null,
+            isLoaded: false,
+            categoryNames: categoryNames,
+            selectedCategoryName: categoryNames[categoryNames.length - 1]
+        };
     }
 
-    this.state = {
-      error: null,
-      isLoaded: false,
-      categoryNames: categoryNames,
-      selectedCategoryName: categoryNames[categoryNames.length - 1],
-      categorySelectedFlag: categorySelectedFlag
-    };
-  }
+    componentDidMount() {
+        fetch(`http://yazilim.vip:9999/${this.props.match.params[0]}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    // howto-service should return error response if content is empty, this check is temporary
+                    if (Object.keys(result).length === 0) {
+                        let error = "error"
+                        this.setState({
+                            error
+                        });
+                    }
 
-  componentDidMount() {
-    fetch(`http://yazilim.vip:9999/${this.props.match.params[0]}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log("result", result)
+                    this.setState({
+                        isLoaded: true,
+                        selectedCategory: result[this.state.selectedCategoryName],
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
-          // howto-service should return error response if content is empty, this check is temporary
-          if(Object.keys(result).length === 0){
-            let error = "sads"
-            this.setState({
-              error
-            });
-          }
+    getBreadcrumbLink(index) {
+        let route = ""
 
-          this.setState({
-            isLoaded: true,
-            selectedCategory: result[this.state.selectedCategoryName],
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+        for (let i = 0; i < index; i++) {
+            route += "/" + this.state.categoryNames[i]
         }
-      )
-  }
 
-  getBreadcrumbLink(index) {
-    let route = ""
-
-    for (var i = 0; i < index; i++) {
-      route += "/" + this.state.categoryNames[i]
+        return route
     }
 
+    render() {
+        const {error, isLoaded, selectedCategory, categoryNames} = this.state;
+        const input = '# This is a header\n\nAnd this is a paragraph';
 
-    console.log("route", route)
-    return route
-  }
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            // console.log("selectedCategory", selectedCategory)
+            // console.log("selectedCategory.subCategoryList", selectedCategory.subCategoryList)
+            // console.log("selectedCategory.howtoList", selectedCategory.howtoList)
 
-  render() {
-    const { error, isLoaded, selectedCategory, categoryNames, categorySelectedFlag } = this.state;
+            return (
+                <Page span={{span: 12}}>
+                    <Breadcrumb>
+                        {
+                            categoryNames.map((item, index) =>
+                                <Breadcrumb.Item
+                                    key={index}
+                                    href={this.getBreadcrumbLink(index + 1)}
+                                    active={index + 1 === categoryNames.length}>
+                                    {item}
+                                </Breadcrumb.Item>
+                            )
+                        }
+                    </Breadcrumb>
+                    <hr/>
 
-    var input = '# This is a header\n\nAnd this is a paragraph'
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
+                    <Row>
+                        {/*Menus*/}
+                        <Col md="3" className="border-right">
+                            <HowToMenu
+                                route={this.props.match.params[0]}
+                                type="subcategory"
+                                title="Sub Categories"
+                                items={selectedCategory.subCategoryList}
+                            />
+                            <br/>
+                            <HowToMenu
+                                route={this.props.match.params[0]}
+                                type="content"
+                                title="Contents"
+                                items={selectedCategory.howtoList}
+                            />
+                        </Col>
 
-      let sideMenuSubCategoryElements = null
-      let sideMenuHowToElements = null
-
-      console.log("selectedCategory", selectedCategory)
-      console.log("selectedCategory.subCategoryList", selectedCategory.subCategoryList)
-      console.log("selectedCategory.howtoList", selectedCategory.howtoList)
-      console.log("categorySelectedFlag", categorySelectedFlag)
-
-      if (selectedCategory.subCategoryList !== undefined && categorySelectedFlag) {
-        sideMenuSubCategoryElements = Object.keys(selectedCategory.subCategoryList).map(key => {
-          return (
-            <ListGroup.Item>
-              <a href={`/howto/${this.props.match.params[0]}/${selectedCategory.subCategoryList[key].name}`}>
-                {selectedCategory.subCategoryList[key].name}
-              </a>
-            </ListGroup.Item>
-          )
-        })
-      }
-
-      if (selectedCategory.howtoList !== undefined && categorySelectedFlag) {
-        sideMenuHowToElements = Object.keys(selectedCategory.howtoList).map((key) =>
-          <ListGroup.Item>{selectedCategory.howtoList[key].label}</ListGroup.Item>
-        )
-      }
-
-      return (
-        <Page span={{ span: 12 }}>
-          <Breadcrumb>
-            {
-              categoryNames.map((item, index) => <Breadcrumb.Item href={this.getBreadcrumbLink(index + 1)} active={index + 1 === categoryNames.length ? true : false}>{item}</Breadcrumb.Item>)
-            }
-          </Breadcrumb>
-          <hr />
-
-          <Row>
-            <Col md="3" className="border-right">
-              <ListGroup>
-                <h5 className="pl-3">Sub Categories</h5>
-                {sideMenuSubCategoryElements}
-
-                <br />
-
-                <h5 className="pl-3">Contents</h5>
-                {sideMenuHowToElements}
-              </ListGroup>
-            </Col>
-
-            <Col md="9">
-              <ReactMarkdown source={input}></ReactMarkdown>
-            </Col>
-          </Row>
-        </Page>
-      );
+                        {/*Content*/}
+                        <Col md="9">
+                            <ReactMarkdown source={input}/>
+                        </Col>
+                    </Row>
+                </Page>
+            );
+        }
     }
-  }
 }
 
 export default HowTo
