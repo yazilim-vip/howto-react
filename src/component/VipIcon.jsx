a597686ef493f1ffea42a54d1b242c2243f59c0e
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-10-10 05:39:36 +0300

    impl: Firebase configs and itself moved to util package.

--- src/component/VipIcon.jsx
+++ src/component/VipIcon.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const YvipIcon = (props) => (
+const VipIcon = (props) => (
   <div className={`d-inline-block yvip-icon ${props.iconCode} ${props.className}`} >
       <FontAwesomeIcon icon={["fab", props.iconCode]} className="mr-3" />
   </div>
 );
 
-export default YvipIcon;
+export default VipIcon;
