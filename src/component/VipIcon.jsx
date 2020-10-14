cb9df062347cb74a7c816a85a5447abdc7aba252
Emre Sen <maemresen@yazilim.vip>
2020-10-15 01:22:19 +0300

    fix: navbar url fixed

--- src/component/VipIcon.jsx
+++ src/component/VipIcon.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const VipIcon = (props) => (
-  <div className={`d-inline-block yvip-icon ${props.iconCode} ${props.className}`} >
+  <div href={props.link} className={`d-inline-block yvip-icon ${props.iconCode} ${props.className}`} >
+    <a href={props.link} target="_blank">
       <FontAwesomeIcon icon={["fab", props.iconCode]} className="mr-3" />
+    </a>
   </div>
 );
 
