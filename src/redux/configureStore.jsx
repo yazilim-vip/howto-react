d4aa2b39a8225b9190787777b20632ad4aedcb05
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-10-17 04:38:29 +0300

    impl: redux-persist configurations

--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -2,17 +2,33 @@ import { createBrowserHistory } from 'history'
 import { applyMiddleware, createStore } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import createRootReducer from './reducers'
+import { composeWithDevTools } from "redux-devtools-extension"
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-import { composeWithDevTools } from "redux-devtools-extension";
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState) {
+    // Middlewares
     const middlewares = [routerMiddleware(history)]
     const middlewareEnhancer = applyMiddleware(...middlewares)
 
+    // Enhancers
     const enhancers = [middlewareEnhancer]
     const composedEnhancers = composeWithDevTools(...enhancers)
 
-    return createStore(createRootReducer(history), preloadedState, composedEnhancers)
+    // Reducers
+    const rootReducer = createRootReducer(history)
+    const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+    // Store & Persistor
+    const store = createStore(persistedReducer, preloadedState, composedEnhancers)
+    const persistor = persistStore(store)
+
+    return { store, persistor }
 }
