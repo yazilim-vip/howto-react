601b8520d48f71678efd8310c85b39502d1d04fd
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-12 16:12:19 +0300

    wip: import and exports organized

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -2,8 +2,8 @@ import * as HowTo from './howto'
 
 export { HowTo }
 export { CustomNavbar } from './customNavbar/CustomNavbar'
-export { PersonCard } from './card/PersonCard'
-export { PortfolioCard } from './card/PortfolioCard'
+export { PersonCard } from './personCard/PersonCard'
+export { PortfolioCard } from './portfolioCard/PortfolioCard'
 export { VipIcon } from './vipIcon/VipIcon'
 export { Page } from './page/Page'
 export { Cv } from './cv/Cv'
