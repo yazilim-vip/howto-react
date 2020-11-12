e232f4802397f3dba557548f167e044bbf4fb872
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-12 04:25:36 +0300

    wip: rest of the components encapsulated with folder

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,7 +1,7 @@
 import PersonCard from './card/PersonCard'
 import PortfolioCard from './card/PortfolioCard'
-import Page from './Page'
-import Cv from './Cv'
+import Page from './page/Page'
+import Cv from './cv/Cv'
 import CustomNavbar from './customNavbar/CustomNavbar'
 import VipIcon from './vipIcon/VipIcon'
 
