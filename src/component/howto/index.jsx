e84c4a60ba759cea5fe0b308333de3934b2cd723
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-13 09:55:58 +0300

    refactor: imports organized

--- src/component/howto/index.jsx
+++ src/component/howto/index.jsx
@@ -1,8 +1,2 @@
 export { HowToArchive } from './HowToArchive'
-export { howToReducer } from './redux/howToReducer'
-export { HOWTO_ACTION_TYPES } from './redux/howToActionTypes'
-export { HOWTO_ACTION_CREATORS } from './redux/howToActionCreators'
-export {
-    HOWTO_VIEW_MODE_GRID_VIEW,
-    HOWTO_VIEW_MODE_LIST_VIEW
-} from './howToConstants'
+export { howToReducer, HOWTO_ACTION_CREATORS } from './redux'
