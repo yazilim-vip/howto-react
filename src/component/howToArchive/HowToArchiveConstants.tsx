6626d7ee29397de99c882c27cfe2820dfac6da30
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-25 20:01:53 +0300

    wip(howto): constants moved
    HowTo archive related conststants moved under howtoArchive comp. folder

--- src/component/howToArchive/HowToArchiveConstants.tsx
+++ src/component/howToArchive/HowToArchiveConstants.tsx
@@ -1,7 +1,9 @@
-export const HOWTO_VIEW_MODE_LIST_VIEW = true
-export const HOWTO_VIEW_MODE_GRID_VIEW = false
-export const HOWTO_DEFAULT_VIEW_MODE = HOWTO_VIEW_MODE_GRID_VIEW
-export type FileManagerViewMode = boolean
+export const HOWTO_VIEW_MODE_LIST_VIEW = 'list-view'
+export const HOWTO_VIEW_MODE_GRID_VIEW = 'grid-view'
+export type FileManagerViewMode =
+| typeof HOWTO_VIEW_MODE_LIST_VIEW
+| typeof HOWTO_VIEW_MODE_GRID_VIEW
+export const HOWTO_DEFAULT_VIEW_MODE: FileManagerViewMode = HOWTO_VIEW_MODE_GRID_VIEW
 
 export const HOWTO_ITEM_TYPE = {
     HOWTO: 'howto',
