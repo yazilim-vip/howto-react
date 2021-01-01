f94e731763a821ea0d7a34033c2d856d5aa904b4
Emre Sen <maemresen@yazilim.vip>
2021-01-01 16:06:24 +0300

    content: emre image updated

--- src/yvip-website/constants/memberList.tsx
+++ src/yvip-website/constants/memberList.tsx
@@ -25,7 +25,7 @@ export const TEAM_MEMBER_LIST: PersonCardProps[] = [
                 </a>
             </>
         ),
-        imageSource: '/img/emre_portrait_cropped.jpeg',
+        imageSource: 'https://avatars2.githubusercontent.com/u/17517090?s=460&u=a0e442b6913f827d2e51e00abe143b2dbc375f33&v=4',
         cvSource: undefined,
         title: 'Major Contributor',
         links: {
