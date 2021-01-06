c58cd302b69b5accf086c83403b060fb7f54cb84
maemresen <maemresen@users.noreply.github.com>
2021-01-06 18:06:38 +0000

    deploy: 1ade00608efb1802e7d8261e4cc7d84747efc9cf

---
+++ precache-manifest.b5ed8d419b14256cbe31393bb7650686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee65072ab1b73fa7db0e0d342fbdd82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "235a79f70d9675b54a90",
+    "url": "/static/css/2.684dff93.chunk.css"
+  },
+  {
+    "revision": "65c89262ca187556b562",
+    "url": "/static/css/main.27f1dc71.chunk.css"
+  },
+  {
+    "revision": "235a79f70d9675b54a90",
+    "url": "/static/js/2.71728def.chunk.js"
+  },
+  {
+    "revision": "a2e032ede5c31980a6d539b1b88b7c46",
+    "url": "/static/js/2.71728def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c89262ca187556b562",
+    "url": "/static/js/main.6cff3376.chunk.js"
+  },
+  {
+    "revision": "e121548ce1d4921e6a20",
+    "url": "/static/js/runtime-main.d3d60aef.js"
+  }
+]);
